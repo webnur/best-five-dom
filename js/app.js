416628f4bcbbeff1f817a6f48c2015da0d51cd39
webnur <mdnuruli291@gmail.com>
2022-08-23 10:21:26 +0600

    app.js modified and project done alhamdulilah

--- js/app.js
+++ js/app.js
@@ -1,62 +1,55 @@
 
 const names = []
 function selectPlayer(player){
-    // console.log(player.parentNode.parentNode.children[0].innerText)
+    // get all select button
     const playerName = player.parentNode.parentNode.children[0].innerText;
+    // get current event button
     const playerId = player;
-    console.log(playerId.parentNode.children.length)
-    // playerId.setAttribute('disabled', true)
 
-   
-    
+    // pass all player name in an array
     names.push(playerName)
     
     // display player name 
     const playerList = document.getElementById('player-list');
     playerList.textContent = '';
-    for(let i = 0; i < names.length; i++){
-        // console.log(names[i])
-        // console.log(i + 1)
-       
 
+    for(let i = 0; i < names.length; i++){
         if( i <= 4){
             const li = document.createElement('li');
             li.innerText = `${i + 1} ${names[i]}`
             playerList.appendChild(li);
+            
+            // disabled select after clicking
             playerId.setAttribute('disabled', true)
             
-        }else{
+        }
+        else{
            playerId.removeAttribute('disabled')
             alert('you already added 5 players')
             break;
-        }
-        
+        }      
     }
 }
 
-
-
-
+// get all input value function
 function getInputvalue(inputId){
     const inputField = document.getElementById(inputId);
     const inputValueString = inputField.value;
     const inputValue = parseFloat(inputValueString);
 
+    // validation input field
     if(isNaN(inputValue)){
         alert('please provide your amount')
     }
     else{
         return inputValue;
-    }
-
-    // return inputValue
-    
+    }   
 }
 
-
+// set all input elemetn in innerHtml
 function setElementvalue(element, value){
     const elementId = document.getElementById(element);
-    // elementId.innerText = value;
+    //validation element field
     if(isNaN(value)){
         elementId.innerText = 0;
     }
@@ -64,16 +57,20 @@ function setElementvalue(element, value){
         elementId.innerText = value;  
     }
 }
+
+// event handler add in per player amount calculation 
 document.getElementById('per-player-amount').addEventListener('click', function(){
    
+    // per player amount
     const playerAmount = getInputvalue('amount-field');
 
+    // get  player quantity list
     const ulLength = document.getElementById('player-list');
     
+    // calculation amount with player quantity
     const playerQuiantity = ulLength.children.length;
     const totalAmount =playerAmount * playerQuiantity;
   
-
     // display player expenses
     setElementvalue('show-player-amount', totalAmount)
 })
@@ -85,6 +82,7 @@ document.getElementById('total-calculate-button').addEventListener('click', func
     const playerExpenses = document.getElementById('show-player-amount');
     const playerExpensesString = playerExpenses.innerText; 
     const playerExpense = parseFloat(playerExpensesString)
+
     // get Manager Amount
     const managerAmount = getInputvalue('manager-amount');
 
@@ -95,9 +93,5 @@ document.getElementById('total-calculate-button').addEventListener('click', func
     const total = playerExpense + managerAmount + coachAmount;
     
     // display total Amount 
-
      setElementvalue('total', total);
-  
-    
-
 })
